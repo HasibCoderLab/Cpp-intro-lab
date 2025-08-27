ff0da7bb4ad1cfdc781f5f52d79e6cc9be857286
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-28 00:18:09 +0600

    add 1st example

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+
+// ================================================
+//               * Language: C++ / Cpp
+//               * Topic: variables
+// =============================================
+
+// ====================1st Example =========================
+#include <iostream>
+using namespace std;
+int main(){
+int x = 5 ;
+cout<<x;
+    return 0;
+}
+
