f3eb78b3ec44a45188166786465979a4066261fd
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-27 23:41:24 +0600

    add code in line_brac file

--- hello.cpp
+++ hello.cpp
@@ -2,14 +2,14 @@
 //               * Language: C++ / Cpp
 //               * Topic: Basic / Intro
 // ===============================================
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main(){
-//     cout<<"Hello Wrold\n";
-//     cout << "Hello Wrold 2.0 \n";
-//     return 0;
-// }
+int main(){
+    cout<<"Hello Wrold\n";
+    cout << "Hello Wrold 2.0 \n";
+    return 0;
+}
 
 
 // ===================== line Break 2nd Option==========================
