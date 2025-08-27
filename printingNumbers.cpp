f515c74f08faacf5d10631f0f37a4bb7432ca47c
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-28 00:08:45 +0600

    update file with code

--- printingNumbers.cpp
+++ printingNumbers.cpp
@@ -2,3 +2,39 @@
 //               * Language: C++ / Cpp
 //               * Topic: printingNumbers
 // ===============================================
+
+// ====================1st Example ===========================
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << 4;
+    return 0;
+}
+
+// ====================2nd Example ===========================
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "4";
+    return 0 ;
+}
+
+// ====================3rd Example ===========================
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout<< 12*3;
+    return 0;
+}
+// ====================4th Example ===========================
+
+#include <iostream>
+using namespace std ;
+int main(){
+  cout<<"12*3" ;
+    return 0;
+}
