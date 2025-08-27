33ece37050d5e8f1c15c7a7f536de75eacf6a9a4
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-27 23:40:01 +0600

    create file

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,25 @@
+// ================================================
+//               * Language: C++ / Cpp
+//               * Topic: Basic / Intro
+// ===============================================
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Hello Wrold\n";
+//     cout << "Hello Wrold 2.0 \n";
+//     return 0;
+// }
+
+
+// ===================== line Break 2nd Option==========================
 #include <iostream>
 using namespace std;
-
 int main(){
-    cout<<"Hello Wrold\n";
-    cout << "Hello Wrold 2.0 \n";
+   cout <<"Hello ! this is 2nd Option";
+   cout<< endl;
+   cout<<"Now see output";
+   cout<< endl;
     return 0;
 }
+
