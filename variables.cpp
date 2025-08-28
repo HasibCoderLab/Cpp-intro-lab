5721136836252f1e0294c4655669f820ebd13e40
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-28 08:08:11 +0600

    add 12th  example of 'variables'in C++

--- variables.cpp
+++ variables.cpp
@@ -7,9 +7,10 @@
 // ==================== 👉👉1st Example =========================
 #include <iostream>
 using namespace std;
-int main(){
-int x = 5 ;
-cout<<x;
+int main()
+{
+    int x = 5;
+    cout << x;
     return 0;
 }
 
@@ -17,103 +18,117 @@ cout<<x;
 
 #include <iostream>
 using namespace std;
-int main(){
-  int x = 3;
-  cout <<x+2;
+int main()
+{
+    int x = 3;
+    cout << x + 2;
     return 0;
 }
 
 // ==================== 👉👉 3rd Example  =========================
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int x = 9;
-    cout<<x-6;
+    cout << x - 6;
     return 0;
 }
 
 // ==================== 👉👉 4th Example  =========================
 
-
 #include <iostream>
 using namespace std;
-int main(){
-int x = 23;
-cout<<x*2;
+int main()
+{
+    int x = 23;
+    cout << x * 2;
     return 0;
-
 }
 // ==================== 👉👉 5th Example  =========================
 
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int x = 6;
-    cout<<6/2;
+    cout << 6 / 2;
     return 0;
 }
-
 
 // ==================== 👉👉 6th Example  =========================
 
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int x = 6;
-    cout<<"6/2"; // output = string
+    cout << "6/2"; // output = string
     return 0;
 }
 
 // ==================== 👉👉 7th Example  =========================
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
-  int x = 10;
-  int y =12;
-  cout << x+y;
+int main()
+{
+    int x = 10;
+    int y = 12;
+    cout << x + y;
     return 0;
 }
 
 // ==================== 👉👉 8th Example  =========================
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
-  int x = 101;
-  int y =12;
-  cout << x-y;
+int main()
+{
+    int x = 101;
+    int y = 12;
+    cout << x - y;
     return 0;
 }
 
 // ==================== 👉👉 9th  Example  =========================
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
-  int x  = 8;
-  int y =12;
-  cout << x*y;
+int main()
+{
+    int x = 8;
+    int y = 12;
+    cout << x * y;
     return 0;
 }
 
 // ==================== 👉👉 10th Example  =========================
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
-  int x = 10;
-  int y =12;
-  cout << x/y;
+int main()
+{
+    int x = 10;
+    int y = 12;
+    cout << x / y;
     return 0;
 }
-
 
 // ==================== 👉👉 11th Example  =========================
-#include <istream>
+#include <iostream>
 using namespace std;
-int main(){
-  int x = 10;
-  int y =12;
-  cout <<" x/y";
+int main()
+{
+    int x = 10;
+    int y = 12;
+    cout << " x%y";
     return 0;
 }
 
+// ==================== 👉👉 12th  Example  =========================
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
 
-
+    cout << " x%4";
+    return 0;
+}
