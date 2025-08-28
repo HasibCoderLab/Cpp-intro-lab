1de7b97557566fd510f421062ed98d9ca54132cb
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-28 09:08:38 +0600

    1st example

---
+++ updationVariables.cpp
@@ -0,0 +1,14 @@
+// ================================================
+//               * Language: C++ / Cpp
+//               * Topic: Updatio-Variables
+// =============================================
+#include <iostream>
+using namespace std;
+int main(){
+  int x = 5;
+  x = 10;
+  cout<<x;
+  cout<<endl;
+    return 0;
+}
+
