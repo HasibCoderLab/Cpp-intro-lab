38110e8597dd663ffc3381327727f3c5d0f8e4e8
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-29 23:14:44 +0600

    update 1st problem

--- solveProblem.cpp
+++ solveProblem.cpp
@@ -1,39 +1,44 @@
 // ================================================
-//               * Language: C++ / Cpp
-//               * Topic: solveProblem
-// =============================================
+//               Language: C++ / Cpp
+//               Topic: Solving Problems
+// ================================================
 
-//======= 👉👉 🔹🔹Question  1️⃣  calculating area of a  Circle  [Unprofessional]
+// ===== 👉👉  🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Unprofessional style example) =====
 #include <iostream>
 using namespace std;
-int main(){
- float r = 5;
- float a = r * r;
- cout<<a*3.1416<<endl;
 
+int main()
+{
+    float r = 5;
+    float a = r * r;
+    cout << "Area is: " << a * 3.1416;
+    cout << endl;
     return 0;
 }
 
-//======= 👉👉 🔹🔹Question  1️⃣  calculating area of a  Circle [simple Professional]
+// ===== 👉👉  🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Professional style example 1) =====
 #include <iostream>
 using namespace std;
-int main(){
- float radius = 4.3;
- cout<< radius*radius*3.1416 <<endl;
+
+int main()
+{
+    float radius = 4.3;
+    cout << "Area is: " << radius * radius * 3.1416;
+    cout << endl;
     return 0;
 }
 
-
-//======= 👉👉 🔹🔹Question  1️⃣  calculating area of a  Circle [simple Professional]
+// ===== 👉👉 🔹🔹 Question 1️⃣ : Calculate the area of a Circle (Professional style example 2) =====
 #include <iostream>
 using namespace std;
-int main(){
- float r = 4.3;
- cout<< r*r*3.1416 <<endl;
+
+int main()
+{
+    float r = 4.3;
+    cout << "Area is: " << r * r * 3.1416;
+    cout << endl;
     return 0;
 }
-
-
 
 // 👉👉  🔹🔹  Question
 // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
