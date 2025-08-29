75dfd10ec88bbe90d8e90e3df2fa95ad2d9d7a9f
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-29 23:37:27 +0600

    solve 2nd problem  in C++

--- solveProblem.cpp
+++ solveProblem.cpp
@@ -40,5 +40,20 @@ int main()
     return 0;
 }
 
+// ===== 👉👉 🔹🔹 Question 2️⃣ Calculating simple interest
+
+#include <iostream>
+using namespace std;
+int main(){
+float p = 60;
+float r = 30;
+float t =  3.2;
+float si = (p*r*t) / 100;
+cout<< "Simle Interest is " <<si;
+cout<<endl;
+
+    return 0;
+}
+
 // 👉👉  🔹🔹  Question
 // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
