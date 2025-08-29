95c506430cd90f2b9c89b80d5698699651d29be5
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-29 21:46:20 +0600

    add 1st example

--- arithmeticOperators.cpp
+++ arithmeticOperators.cpp
@@ -4,8 +4,15 @@
 //               * Topic: arithmeticOperators
 // =============================================
 
+// =============================👉👉 1st Example ================
+
 #include <iostream>
 using namespace std;
 int main(){
+    int x = 10,  y= 3;
+    cout<< x+y<<endl;
+    cout<<x-y<<endl;
+    cout<<x*y<<endl;
+    cout<<x/y <<endl;
     return 0;
 }
