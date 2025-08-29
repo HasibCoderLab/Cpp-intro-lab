d394d9ce52f02578f8d881fb096404105899675e
HasibCoderLab <coder.mohammadhasibhasan@gmail.com>
2025-08-29 22:41:38 +0600

    solve 1st example [simple version] in C++

---
+++ solveProblem.cpp
@@ -0,0 +1,21 @@
+// ================================================
+//               * Language: C++ / Cpp
+//               * Topic: solveProblem
+// =============================================
+
+//======= 👉👉 🔹🔹Question  1️⃣  calculating area of a  Circle 
+#include <iostream>
+using namespace std;
+int main(){
+ int radius = 5;
+ int area = radius * radius;
+ cout<<area*3.1416 <<endl;
+
+    return 0;
+}
+
+
+
+
+// 👉👉  🔹🔹  Question
+// 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
